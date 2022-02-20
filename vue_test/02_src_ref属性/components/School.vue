10bfd8648ffb42d8240c04e1c71f99e172ac6724
ChenXander <cx1197257655@gmail.com>
2022-02-20 14:04:14 +0800

    vue_test

---
+++ vue_test/02_src_ref属性/components/School.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="demo">
+        <h2>学校名称：{{name}}</h2>
+        <h2>学校地址：{{address}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'School',
+    data() {
+        return {
+            name: 'VUE',
+            address: '北京',
+        }
+    },
+}
+</script>
+
+<style>
+    .demo {
+        background-color: pink;
+    }
+</style>
